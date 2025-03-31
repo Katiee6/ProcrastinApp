7ee151fc750bbcfd092f187aa542fcce6523a7a9
Katiee6 <katia.bouarab@toulouse.miage.fr>
2025-03-31 11:15:02 +0200

    First commit

---
+++ src/main/java/fr/miage/toulouse/m1appentreprise/procrastinapp/procrastinapp/ProcrastinAppApplication.java
@@ -0,0 +1,13 @@
+package fr.miage.toulouse.m1appentreprise.procrastinapp.procrastinapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcrastinAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProcrastinAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/miage/toulouse/m1appentreprise/procrastinapp/procrastinapp/ProcrastinAppApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.miage.toulouse.m1appentreprise.procrastinapp.procrastinapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProcrastinAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
